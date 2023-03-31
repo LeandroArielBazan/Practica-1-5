65dad2a55e474add2403b9231da9af377b467645
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-03-31 20:03:28 -0300

    practica 1-5

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+document.querySelector ("body").innerHTML = `
+
+<h1>Como encender una playstation 4</h1>
+<ol>
+    <li>buscar joystick</li>
+    <li>apretar boton PS del joystick emparejado</li>
+    <li>encender la televisión</li>
+    <li>comprobar si encendio correctamente</li>
+    <li> <img src="https://www.wikihow.com/images/d/dc/Turn-a-PlayStation-4-on-Step-7.jpg" alt=""></li>
+</ol> 
+<h3> Leandro Ariel Bazan </h3> `
+
